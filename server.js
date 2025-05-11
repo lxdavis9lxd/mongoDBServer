62279e3ca8a16bc303c15f36a687ad710f261e7e
lxdavis9lxd <lxdavis9lxd@gmail.com>
2025-05-11 06:45:54 +0000

    committed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware
 app.use(bodyParser.json());
